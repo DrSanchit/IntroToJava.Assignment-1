565c8b1588c08d7229b67ca2ed37df40af792797
DrSanchit <sanchit.bhatnagar@hotmail.com>
2016-07-30 01:11:14 +0530

    Assignment-1.1
    The Sum of two Integers

---
+++ Sum.java
@@ -0,0 +1,17 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+	
+		int a = 10; 			// integer declaration
+		float b = 10.5f;        //float declaration
+		
+		float c=a+b;			//Storing the sum of a+b value to Variable c
+		
+		int d = (int)c;		//explicit type-casting
+		
+		System.out.println("The Sum Of Two numbers ="+""+d);
+
+	}
+
+}
